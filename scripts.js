8845046f201cc2368970b557367ccb40b1e32eec
Ramona Stanberry <monacstanberry@gmail.com>
2021-10-23 11:46:24 -0500

    Colour, Font and Layout style changed

--- scripts.js
+++ scripts.js
@@ -1,1 +1,15 @@
 /* Add your JavaScript to this file */
+onload=function(){
+    var email= document.querySelector('#email');
+    var message= document.querySelector('.message');
+    this.addEventListener('submit',event=>{
+        event.preventDefault();
+        if (email.value!=''){
+            message.innerHTML=`Thank you! your email address ${email.value} has been added to our mailing list`;
+            email.value='';
+        }else{
+            message.innerHTML='Please enter a valid email address';
+            email.value='';
+        }
+    });
+}
